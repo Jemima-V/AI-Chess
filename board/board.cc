e2c0597c9b058f284c5c9e911478c76e00cedd05
j8vijaya <j8vijaya@uwaterloo.ca>
2022-11-28 19:53:55 -0500

    pieceAt and few other board.cc functions

--- board/board.cc
+++ board/board.cc
@@ -13,76 +13,64 @@
 Board:: Board(){
     //currBoard(8, (vector<Piece*>(8, nullptr))); 
     for (int i = 0; i < 8; i++){
+        //currBoard.push_back(std::vector<Piece*>{});
         for(int j = 0; j < 8; j++){
+            //currBoard[1].emplace_back(nullptr);
             currBoard[i][j] = nullptr;
             //currBoard.emplace_back(nullptr)
         }
     }
 }
 
+  //dtor 
+Board:: ~Board(){
+    //iterate over the 2D vector and delete each piece pointer it contains
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            delete currBoard[i][j];
+        }
+    }
+
+    delete currBoard;
+}
+
 void Board:: initBoard(){
     int white = 1; //player 1
     int black = 2; //player 2
-
-    //Piece IDs: first digit represents the colour, 2nd digit represents the type of piece
-    // rooks => 1, Knights =>2, Bishops => 3, Kings => 4, Queens => 5, Pawns => 6
-    // these will be usefull when rendering and we need to know Piece types
-    /*
-    int whiteRookID = 11;
-    int whiteKnightID = 12;
-    int whiteBishopID = 13;
-    int whiteKingID = 14;
-    int whiteQueenID = 15;
-    int whitePawnID = 16;
-
-    int blackRookID = 21;
-    int blackKnightID = 22;
-    int blackBishopID = 23;
-    int blackKingID = 24;
-    int blackQueenID = 25;
-    int blackPawnID = 26;
-
-    currboard[0][0]= new Rook;
-    currboard[0][1]= new Knight;
-    currboard[0][2]= new Bishop;
-    currboard[0][3]= new King;
-    currboard[0][4]= new Queen;
-    currboard[0][5]= new Bishop;
-    currboard[0][6]= new Knight;
-    currboard[0][7]= new Rook;
-    */
     
     //WHITE SIDE
     for(int i = 0; i < 7; i++){
 
         //Rooks
         if(i == 0 || i == 7){
-            currBoard[6][i]= new Rook{white, false, 'R'};
+            currBoard[7][i]= new Rook{white, false, 'R'};
         
         //Knights
         } else if (i == 1 || i == 6){
-            currBoard[6][i]= new Knight{white, false, 'N'};
+            currBoard[7][i]= new Knight{white, false, 'N'};
 
         //Bishops
-        } else if (i == 2 || i = 5){
-            currBoard[6][i]= new Bishop{white, false, 'B'};
+        } else if ( i == 2 || i == 5){
+            currBoard[7][i]= new Bishop{white, false, 'B'};
         
         //King
         } else if (i == 4){
-            currBoard[6][i]= new King{white, false, 'K'};
+            currBoard[7][i]= new King{white, false, 'K'};
+            whiteKing.rank = 7;
+            whiteKing.file = i;
         
         //Queen
         } else {
-            currBoard[6][i]= new Queen{white, false, 'Q'};
+            currBoard[7][i]= new Queen{white, false, 'Q'};
         }
     }
 
     //Pawns
     for(int i = 0; i < 8; i++){
-        currBoard[6][i]= new Pawn{white, false, "P", true, false};
+        currBoard[6][i]= new Pawn{white, false, 'P', true, false};
     }
 
-    //BLACK SIDE -> owner = player1
+    //BLACK SIDE
     for(int i = 0; i < 7; i++){
 
         //Rooks
@@ -94,13 +82,15 @@ void Board:: initBoard(){
             currBoard[0][i]= new Knight{black, false, 'n'};
 
         //Bishops
-        } else if (i == 2 || i = 5){
+        } else if ( i == 2 || i == 5){
             currBoard[0][i]= new Bishop{black, false, 'b'};
         
         //King
         } else if (i == 3){
             currBoard[0][i]= new King{black, false, 'k'};
-        
+            blackKing.rank = 0;
+            blackKing.file = i;
+
         //Queen
         } else {
             currBoard[0][i]= new Queen{black, false, 'q'};
@@ -108,7 +98,7 @@ void Board:: initBoard(){
     }
 
     //Pawns
-    for(int i = 0; i < 8; i++){
+    for (int i = 0; i < 8; i++){
         currBoard[1][i]= new Pawn{black, false, 'p', true, false};
     }
 
@@ -116,35 +106,75 @@ void Board:: initBoard(){
 }
 
   //Get the state of each position on the board for rendering
-  char Board:: getState(int rank, int file) const{
+char Board:: getState(int rank, int file) const{
     if(currBoard[rank][file] != nullptr){
-        int id = currBoard[rank][file]->getId();
+        char id = currBoard[rank][file]->getId();
         return id;
     } else{
         return 'X';
     }
-  }
+}
 
   /*
   &Board getCurrBoard(){
     return *currBoard;
   }
   */
+Pieces* Board:: PieceAt(Position pos){
+    return currBoard[pos.rank][pos.file];
+}
 
+Position Board:: getWhiteKing(){
+    return whiteKing;
+}
+
+Position Board:: getBlackKing(){
+    return BlackKing;
+}
+
+bool Board:: checkRow(Position from, Position to){
+
+    //Make sure kriti checks start and from are not the same position
+    if (from.file < to.file){
+        for(int i = from.file, i <= to.file; i++){
+        if currBoard[][]
+    }
+    }
+    
+}
+
+bool Board:: checkCol(Position from, Position to){
+
+}
+
+bool Board:: checkDiag(Position from, Position to){
+
+}
+
+Pieces* Board:: checkRowOpp(int owner, Position from, Position to){
+
+}
+
+Pieces* Board:: checkColOpp(int owner, Position from, Position to){
+
+}
+
+Pieces* Board:: checkDiag(int owner, Position from, Position to){
+
+}
+
+
+
+
+
+
+
+ //TALK OVER WITH MALVIKA
   //alter the main board to reflect the move -> set old location to null, new position to the piece
-  void makeMove(Piece *p, Position posOld, Position posNew){
+  void Board:: makeMove(Pieces *p, Position posOld, Position posNew){
     currBoard[posNew.rank][posNew.file] = p;
     currBoard[posOld.rank][posOld.rank] = nullptr;
   }
-  
-  //dtor 
-  Board:: ~Board(){
-    //iterate over the 2D vector and delete each piece pointer it contains
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            delete currBoard[i][j];
-        }
-    }
 
-    delete currBoard;
-  }
+  
+
--- board/board.h
+++ board/board.h
@@ -4,15 +4,19 @@
 #include <vector>
 #include "pieces.h"
 #include "subject.h"
+//using namespace std;
 
 class Board: public Subject{
 
   //2D vector of piece pointers to create a board
   //initalize to 8 "rows" of vectors with 8 "Columns" containing nullptrs 
-  //std::vector<std::vector<Piece*>> currBoard(8, std::vector<Piece*>(8, nullptr));
+  //vector<vector<Pieces*>> currBoard(8, vector<Pieces*>(8, nullptr));
   //std::vector<vector<Piece*>> currBoard;
+  
+  Position whiteKing;
+  Position blackKing;
+  Pieces* currBoard[8][8];
 
-  Piece *currBoard[8][8];
 
  public:
 
@@ -25,14 +29,33 @@ class Board: public Subject{
   void initBoard();
 
   //Get the state of each position on the board for rendering
-  char getState() const;
+  char getState(int rank, int file) const;
 
   //Get the whole board
-  &Board getCurrBoard();
+  //&Board getCurrBoard();
+
+  Pieces* pieceAt(Position pos);
 
   //alter the main board to reflect the move -> set old location to null, new position to the piece
-  void makeMove(Piece *p, Position posOld, Position posNew);
+  void makeMove(Pieces *p, Position posOld, Position posNew);
   
+  Position getWhiteKing();
+
+  Position getBlackKing();
+
+  bool checkRow(Position from, Position to);
+
+  bool checkCol(Position from, Position to);
+
+  bool checkDiagonal(Position from, Position to);
+
+  Piece* checkRowOpp(int owner, Position from, Position to);
+
+  Piece* checkColOpp(int owner, Position from, Position to);
+
+  Peice* checkDiagOpp(int owner, Position from, Position to);
+
+
   /*
   void Board();
   void Display();
