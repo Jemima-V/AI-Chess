8141c17d8f06ffa7a8e2b071ccaf78c929df688d
k5sodhi <k5sodhi@uwaterloo.ca>
2022-12-03 09:03:46 -0500

    fixed the bug in the pawn validMove method

--- pieces/pawn.cc
+++ pieces/pawn.cc
@@ -14,7 +14,6 @@ void Pawn::setFirstMove(bool newMove) {
 
 // checks if there is a piece of the other player diagonal to the pawn
 bool Pawn::potentialCapture(Position start, Position end, Board* board) const {
-    cout << "check diagonal" << endl;
     Pieces* pieceToKill = board->pieceAt(end);
     if (pieceToKill != nullptr && (pieceToKill->getOwner() != (board->pieceAt(start)->getOwner()))) {
         // there is another player's piece there and the pawn can now capture 
@@ -26,12 +25,21 @@ bool Pawn::potentialCapture(Position start, Position end, Board* board) const {
 bool Pawn::validMove(Position start, Position end, Board* board) const {
     Pieces* currPiece = board->pieceAt(start);
     if (owner == 1) {
-        if (firstMove == true) { // move forward 2 squares only at starting position
+        if (potentialCapture(start, end, board) == true) { // move diagonally forward only to capture
+            // can be the left diagonal or the right diagonal
+            // rank must be +1, file must be one of +1 or -1
+            int rankChange = end.rank - start.rank;
+            int fileChange = end.file - start.file;
+            if (rankChange == 1 && (fileChange == 1 || fileChange == -1)) {
+                return true;
+            }
+        } else if (firstMove == true) { // move forward 2 squares only at starting position
             // rank changes by +1 or +2, file must stay the same
             int rankChange = end.rank - start.rank;
             if ((start.file == end.file) && (rankChange == 1)) {
                 return true;
-            } else if ((start.file == end.file) && (rankChange == 2)) {
+            } 
+            if ((start.file == end.file) && (rankChange == 2)) {
                 // check that it doesn't jump over other pieces
                 Position midCheck{start.file, start.rank + 1};
                 if (currPiece->checkBounds(midCheck) == false) {
@@ -43,33 +51,30 @@ bool Pawn::validMove(Position start, Position end, Board* board) const {
                 } else {
                     return true;
                 }
-            } else {
-                return false;
-            }
-        } else if (potentialCapture(start, end, board) == true) { // move diagonally forward only to capture
-            // can be the left diagonal or the right diagonal
-            // rank must be +1, file must be one of +1 or -1
-            int rankChange = end.rank - start.rank;
-            int fileChange = end.file - start.file;
-            if (rankChange == 1 && (fileChange == 1 || fileChange == -1)) {
-                return true;
             }
         } else { // can only move forward 1 position
             // rank changes by +1, file must stay the same
             int rankChange = end.rank - start.rank;
             if ((start.file == end.file) && (rankChange == 1)) {
                 return true;
-            } else {
-                return false;
             }
         }
     } else { // owner must be 2
-        if (firstMove == true) { // move forward 2 squares only at starting position
+        if (potentialCapture(start, end, board) == true) { // move diagonally forward only to capture
+            // can be the left diagonal or the right diagonal
+            // rank must be -1, file must be one of +1 or -1
+            int rankChange = end.rank - start.rank;
+            int fileChange = end.file - start.file;
+            if (rankChange == -1 && (fileChange == 1 || fileChange == -1)) {
+                return true;
+            }
+        } else if (firstMove == true) { // move forward 2 squares only at starting position
             // rank changes by -1 or -2, file must stay the same
             int rankChange = end.rank - start.rank;
             if ((start.file == end.file) && (rankChange == -1)) {
                 return true;
-            } else if ((start.file == end.file) && (rankChange == -2)) {
+            }
+            if ((start.file == end.file) && (rankChange == -2)) {
                 // check that it doesn't jump over other pieces
                 Position midCheck{start.file, start.rank - 1};
                 if (currPiece->checkBounds(midCheck) == false) {
@@ -81,24 +86,12 @@ bool Pawn::validMove(Position start, Position end, Board* board) const {
                 } else {
                     return true;
                 }
-            } else {
-                return false;
-            }
-        } else if (potentialCapture(start, end, board) == true) { // move diagonally forward only to capture
-            // can be the left diagonal or the right diagonal
-            // rank must be -1, file must be one of +1 or -1
-            int rankChange = end.rank - start.rank;
-            int fileChange = end.file - start.file;
-            if (rankChange == -1 && (fileChange == 1 || fileChange == -1)) {
-                return true;
             }
         } else { // can only move forward 1 position
             // rank changes by -1, file must stay the same
             int rankChange = end.rank - start.rank;
             if ((start.file == end.file) && (rankChange == -1)) {
                 return true;
-            } else {
-                return false;
             }
         }
     }
